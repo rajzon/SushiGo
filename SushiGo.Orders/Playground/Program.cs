28d885e323215fa8e99ff7ea86e261abc662e451
Dawid <hms.basilisk@gmail.com>
2025-02-25 20:34:42 +0100

    .

---
+++ SushiGo.Orders/Playground/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(Test.Run());
+
+public interface ISuccessResponse<T>
+{
+    T Data { get; }
+}
+
+public interface IFailureResponse<T>
+{
+    string Error { get; }
+}
+
+public class ErrorOr<T> : ISuccessResponse<T>, IFailureResponse<T>
+{
+    public bool IsSuccess { get;}
+    public T? Data { get; }
+    public string? Error { get; }
+
+    private ErrorOr(T data)
+    {
+        IsSuccess = true;
+        Data = data;
+    }
+
+    private ErrorOr(string error)
+    {
+        IsSuccess = false;
+        Error = error;
+    }
+
+    public static ErrorOr<T> Success(T data) => new(data);
+
+    public static ErrorOr<T> Failure(string error) => new(error);
+}
+
+
+public static class Test
+{
+    public static int Run()
+    {
+        // test.Data = test.Data + 1;
+        var result = ErrorOr<int>.Success(10);
+        var result2 = ErrorOr<int>.Failure("failure message");
+
+        return result.Data;
+    }
+}
+
+//Proxy
---
+++ SushiGo.Orders/SushiGo.Orders.Core/Sushi.cs
@@ -0,0 +1,16 @@
+﻿namespace SushiGo.Orders.Core;
+
+public abstract class AuditableEntity
+{
+    //TODO maybe generate key
+    public int Id { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime UpdatedAt { get; private set; }
+    public bool IsDeleted { get; private set; }
+}
+
+public sealed class Order : AuditableEntity
+{
+    public string Name { get; }
+}
+
